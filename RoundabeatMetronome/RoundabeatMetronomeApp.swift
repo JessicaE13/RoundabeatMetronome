111a2623b198c89b20a6e5f6671aedd1a025bfbb
Jessica Estes <jessica.m.estes@gmail.com>
2025-04-13 15:07:55 -0400

    Initial Commit

---
+++ RoundabeatMetronome/RoundabeatMetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoundabeatMetronomeApp.swift
+//  RoundabeatMetronome
+//
+//  Created by Jessica Estes on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct RoundabeatMetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
