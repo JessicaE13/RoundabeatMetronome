8afd8b9a95f5fdde944b42a5b10e75bc9eafa13f
Jessica Estes <jessica.m.estes@gmail.com>
2025-07-13 13:44:18 -0400

    Add Constants file

---
+++ RoundabeatMetronome/Constants.swift
@@ -0,0 +1,13 @@
+//Constants.swift
+
+import Foundation
+
+//Configuration file for your app's RevenueCat settings.
+
+struct Constants {
+    
+    static let apiKey = "appl_bjboiZRJoRJDLDsmrANaGgsXWzR"
+    
+    static let entitlementId = "Pro"
+    
+}
